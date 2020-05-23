f8a7d42cf2e6af69bfc8f7c9ccba9232b519a65c
FadeevSergey <bboottaayy@gmail.com>
2020-05-23 18:38:34 +0300

    fix

--- stuff/divide/test.c
+++ stuff/divide/test.c
@@ -4,8 +4,10 @@ int d(int);
 
 int main() {
 
-	for(int i = -10; i < 20; i++) {
-		printf("%d >> 1 = %d\n", i, d(i));
+	for(int i = -5; i < 5; i++) {
+		printf("%d >> -1 = %d\n", i, i >> -1);
 	}
+	10 >> 1;
+	//printf("%d", 10 >> -1);
 
 }
